d37a2607b5fa833988ef0754ae1fd604123525b1
achaves100 <adan.chaves@outlook.com>
2021-04-14 23:00:49 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
-
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
@@ -12,4 +8,22 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const SuperForm = () => {
+	const [currentValue, setValue] = useState("");
+	return (
+		<div>
+			<h2>Your name is {currentValue ? currentValue : "Not defined"}</h2>
+			<input
+				type="text"
+				onChange={e => setValue(e.target.value)}
+				value={currentValue}
+				placeholder="Please type your name"
+			/>
+		</div>
+	);
+};
+ReactDOM.render(<SuperForm />, document.getElementById("app"));
